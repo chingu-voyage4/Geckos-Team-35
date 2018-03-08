6588d099c9b170eda5ced6c9f715d8914aed4c2a
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-03-08 18:58:10 -0400

    Import todo class to todo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../classes/todo';
 
 @Injectable()
 export class TodoService {
+
+  private todos: Todo[];
 
   constructor() { }
 
