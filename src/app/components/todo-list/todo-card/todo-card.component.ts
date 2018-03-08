aa081283a65e1087e5849b89e4c5395ba6bd4210
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-03-08 19:14:01 -0400

    Import the Todo Service to the todo  card component

--- src/app/components/todo-list/todo-card/todo-card.component.ts
+++ src/app/components/todo-list/todo-card/todo-card.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../../services/todo.service';
 
 @Component({
   selector: 'app-todo-card',
   templateUrl: './todo-card.component.html',
   styleUrls: ['./todo-card.component.css']
 })
+
 export class TodoCardComponent implements OnInit {
 
   public todoText: string;
   isClassVisible: false;
+
   constructor() { this.todoText = ''; }
 
   ngOnInit() {
