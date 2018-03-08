905c1e5125a507825bcdc820127412dc1303c4df
LuisJaquez <luismanueljaquezpena@gmail.com>
2018-03-08 19:08:03 -0400

    Add  todo add, get todos and remove todo functions in the todo services

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -5,7 +5,29 @@ import { Todo } from '../classes/todo';
 export class TodoService {
 
   private todos: Todo[];
+  private nextId: number;
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      new Todo(0, 'Make dinner tonight!'),
+      new Todo(1, 'Fold the laundry.'),
+      new Todo(2, 'Learn to make a React app!')
+    ];
 
+    this.nextId = 3;
+  }
+
+  public addTodo(text: string): void {
+    const todo = new Todo(this.nextId, text);
+    this.todos.push(todo);
+    this.nextId++;
+  }
+
+  public getTodos(): Todo[] {
+    return this.todos;
+  }
+
+  public removeTodo(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 }
